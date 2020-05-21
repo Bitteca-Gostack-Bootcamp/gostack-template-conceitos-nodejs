3c6e2ae19b40e8a5586960960e0f32083f453110
Eduardo Bittencourt <biteca@gmail.com>
2020-05-21 14:15:14 -0300

    Final commit

--- src/app.js
+++ src/app.js
@@ -8,26 +8,73 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-const repositories = []
+let repositories = []
 
 app.get('/repositories', (request, response) => {
   return response.json(repositories)
 })
 
 app.post('/repositories', (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body
+
+  const repository = { id: uuid(), title, url, techs, likes: 0 }
+  repositories = [...repositories, repository]
+
+  return response.json(repository)
 })
 
 app.put('/repositories/:id', (request, response) => {
-  // TODO
+  const { id } = request.params
+
+  const { title, url, techs } = request.body
+
+  const repositoryIndex = repositories.findIndex(
+    (repository) => repository.id === id
+  )
+
+  if (repositoryIndex < 0) {
+    return response.status(400).json({ error: 'Repository does not exists' })
+  }
+  const likes = repositories[repositoryIndex].likes
+  const repository = { id, title, url, techs, likes }
+  repositories[repositoryIndex] = repository
+
+  return response.json(repository)
 })
 
 app.delete('/repositories/:id', (request, response) => {
-  // TODO
+  const { id } = request.params
+
+  const repositoryIndex = repositories.findIndex(
+    (repository) => repository.id === id
+  )
+
+  if (repositoryIndex < 0) {
+    return response.status(400).json({ error: 'Repository does not exists' })
+  }
+
+  repositories.splice(repositoryIndex, 1)
+
+  return response.status(204).send()
 })
 
 app.post('/repositories/:id/like', (request, response) => {
-  // TODO
+  const { id } = request.params
+  const repositoryIndex = repositories.findIndex(
+    (repository) => repository.id === id
+  )
+
+  if (repositoryIndex < 0) {
+    return response.status(400).json({ error: 'Repository does not exists' })
+  }
+  const { title, url, techs } = repositories[repositoryIndex]
+
+  let likes = repositories[repositoryIndex].likes
+  likes += 1
+  const repository = { id, title, url, techs, likes }
+  repositories[repositoryIndex] = repository
+
+  return response.json(repository)
 })
 
 module.exports = app
